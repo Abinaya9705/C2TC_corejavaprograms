b7b5075789d71782409d24ec4e05bc693e3d7b8e
Abinaya <abiss9705@gmail.com>
2025-08-14 19:48:41 +0530

    i have added new encapsulation program.

--- corejavaprograms/src/day1_basics/HallowSquare.java
+++ corejavaprograms/src/day1_basics/HallowSquare.java
@@ -1,11 +1,11 @@
 package day1_basics;
-
+import java.util.*;
 public class HallowSquare {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the number of rows: ");
-        int n = scanner.nextInt();
+		Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int n = sc.nextInt();
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
                 if (i == 1 || i == n || j == 1 || j == n) {
