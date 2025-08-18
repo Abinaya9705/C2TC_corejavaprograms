281e3419de4aa731d93ad56a20a27d44c8b332c2
Abinaya <abiss9705@gmail.com>
2025-08-18 11:11:27 +0530

    I have edited the code.

--- corejavaprograms/src/com/tnsif/daythree/Customer.java
+++ corejavaprograms/src/com/tnsif/daythree/Customer.java
@@ -2,58 +2,60 @@ package com.tnsif.daythree;
 
 public class Customer {
 
-	public static void main(String[] args) {
-		private String customerName;
-		 private int customerId;
-		 private String customerCity;
-		 
-		// Default Constructor
-		public Customer() {
-			System.out.println("Default Constructor");
-		}
-		
-		//Parameterized Constructor
-		//use of this keyword
-		public Customer(String customerName, int customerId, String customerCity) {
-			
-			this(); //calling default constructor
-			
-			System.out.println("Parameterized Constructor");
-			this.customerName = customerName;
-			this.customerId = customerId;
-			this.customerCity = customerCity;
-		}
-		
-		//Getter and Setters
-		public String getCustomerName() {
-			return customerName;
-		}
+    // Instance variables (correct position)
+    private String customerName;
+    private int customerId;
+    private String customerCity;
 
-		public void setCustomerName(String customerName) {
-			this.customerName = customerName;
-		}
+    // Default Constructor
+    public Customer() {
+        System.out.println("Default Constructor");
+    }
 
-		public int getCustomerId() {
-			return customerId;
-		}
+    // Parameterized Constructor
+    public Customer(String customerName, int customerId, String customerCity) {
+        this(); // calling default constructor
+        System.out.println("Parameterized Constructor");
+        this.customerName = customerName;
+        this.customerId = customerId;
+        this.customerCity = customerCity;
+    }
 
-		public void setCustomerId(int customerId) {
-			this.customerId = customerId;
-		}
+    // Getters and Setters
+    public String getCustomerName() {
+        return customerName;
+    }
 
-		public String getCustomerCity() {
-			return customerCity;
-		}
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
 
-		public void setCustomerCity(String customerCity) {
-			this.customerCity = customerCity;
-		}
+    public int getCustomerId() {
+        return customerId;
+    }
 
-		@Override
-		public String toString() {
-			return "Customer [customerName=" + customerName + ", customerId=" + customerId + ", customerCity="
-					+ customerCity + "]";
-	    }
-		
-	}
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerCity() {
+        return customerCity;
+    }
+
+    public void setCustomerCity(String customerCity) {
+        this.customerCity = customerCity;
+    }
+
+    // toString() method for printing details
+    @Override
+    public String toString() {
+        return "Customer [Id=" + customerId + ", Name=" + customerName + ", City=" + customerCity + "]";
+    }
+
+    // Main method (if you want to test here separately)
+    public static void main(String[] args) {
+        Customer c1 = new Customer();
+        Customer c2 = new Customer("John", 101, "Chennai");
+        System.out.println(c2);
+    }
 }
